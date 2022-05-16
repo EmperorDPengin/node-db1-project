ab04755c7741ebc42108f526c70d649a6321945b
Emperor D. Pengin <emperord.pengin@gmail.com>
2022-05-16 19:52:44 -0400

    serverjs completed server running

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 
 const server = express();
+const accountsRouter = require('./accounts/accounts-router');
 
 server.use(express.json());
+
+server.use('/api/accounts', accountsRouter);
+
+server.use('*', (req, res) => {
+    res.status(404).json( {message: `${req.method} ${req.baseUrl} not found`});
+});
+
 
 module.exports = server;
